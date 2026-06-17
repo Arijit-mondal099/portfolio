117e8ab85f634df489961db0fbecdfe0a5120063
Arijit Mondal <arijitm717@gmail.com>
2026-06-17 16:38:54 +0530

    fix: make project card hover smooth by animating transform only

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -34,7 +34,7 @@ function ProjectLink({
  */
 export function ProjectCard({ project }: { project: Project }) {
   return (
-    <Card className="h-full transition-[transform,box-shadow] duration-400 ease-[cubic-bezier(0.22,1,0.36,1)] will-change-transform hover:shadow-lg motion-safe:hover:-translate-y-1">
+    <Card className="h-full transform-gpu transition-transform duration-300 ease-out will-change-transform motion-safe:hover:-translate-y-1.5">
       <Image
         src={project.image}
         alt={project.title}
